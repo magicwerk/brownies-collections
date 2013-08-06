62c891571fc49b3948dc3fbac60483330d5aa145
origo <origo@d951b014-a73d-4a8f-a781-beef09d2eb48>
2013-08-06 23:44:09 +0000

    - work in progress
    git-svn-id: svn://localhost/magicwerk.origo/trunk@1810 d951b014-a73d-4a8f-a781-beef09d2eb48

--- src/org/magicwerk/brownies/collections/MapList.java
+++ src/org/magicwerk/brownies/collections/MapList.java
@@ -23,9 +23,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import org.magicwerk.brownies.collections.KeyList.Handler;
+import org.magicwerk.brownies.collections.KeyList.BuilderBase;
+import org.magicwerk.brownies.collections.KeyList.DuplicateMode;
+import org.magicwerk.brownies.collections.KeyList.Trigger;
 import org.magicwerk.brownies.collections.KeyList.NullMode;
+import org.magicwerk.brownies.collections.KeyList.BuilderBase.KeyMapBuilder;
 import org.magicwerk.brownies.collections.SetList.Builder;
+import org.magicwerk.brownies.collections.SetList.IdentMapper;
 
 
 /**
@@ -79,18 +83,17 @@ public class MapList<E, K> extends KeyList<E> {
 	}
 
 
-	/**
-     * Builder to construct MapList instances.
+    /**
+     * Builder to construct SetList instances.
      */
-    public static class Builder<E, K> extends KeyList.Builder<E> {
-
+    public static class Builder<E, K> extends BuilderBase<E> {
         /**
          * Constructor.
          *
          * @param mapper mapper to use
          */
-        public Builder(Mapper<E, K> mapper) {
-        	withKey((Mapper<E, Object>) mapper);
+        public Builder(Mapper<E,K> mapper) {
+        	newKeyMapBuilder((Mapper<E, Object>) mapper);
         }
 
         /**
@@ -98,48 +101,159 @@ public class MapList<E, K> extends KeyList<E> {
          *
          * @param mapList   mapList to customize
          */
-        Builder(MapList<E, K> mapList) {
+        Builder(MapList<E,K> mapList) {
             this.keyList = mapList;
         }
 
         /**
-         * Build MapList with specified options.
+         * Specify insert trigger.
          *
-         * @return  MapList with specified options
+         * @param trigger	insert trigger method
+         * @return			this (fluent interface)
          */
-        public MapList<E, K> build() {
-            return (MapList<E, K>) build();
+        public Builder<E,K> withInsertTrigger(Trigger<E> trigger) {
+        	endKeyMapBuilder();
+            this.insertTrigger = trigger;
+            return this;
         }
 
-        // --- Methods overridden to change return type
-
-		@Override
-        public Builder<E, K> withInsertTrigger(Handler<E> handler) {
-            return (Builder<E, K>) super.withInsertTrigger(handler);
+        /**
+         * Specify delete trigger.
+         *
+         * @param trigger	delete trigger method
+         * @return			this (fluent interface)
+         */
+        public Builder<E,K> withDeleteTrigger(Trigger<E> trigger) {
+        	endKeyMapBuilder();
+            this.deleteTrigger = trigger;
+            return this;
         }
 
-		@Override
-        public Builder<E, K> withDeleteTrigger(Handler<E> handler) {
-            return (Builder<E, K>) super.withDeleteTrigger(handler);
+        //-- Capacity / Elements
+
+        public Builder<E,K> withCapacity(int capacity) {
+        	endKeyMapBuilder();
+            this.capacity = capacity;
+            return this;
         }
 
-		@Override
-		public Builder<E, K> withCapacity(int capacity) {
-			return (Builder<E, K>) super.withCapacity(capacity);
-		}
+        public Builder<E,K> withElements(Collection<? extends E> elements) {
+        	endKeyMapBuilder();
+            this.collection = elements;
+            return this;
+        }
 
-		@Override
-		public Builder<E, K> withElements(Collection<? extends E> elements) {
-			return (Builder<E, K>) super.withElements(elements);
-		}
+        public Builder<E,K> withElements(E... elements) {
+        	endKeyMapBuilder();
+            this.array = elements;
+            return this;
+        }
 
-		@Override
-		public Builder<E, K> withElements(E... elements) {
-			return (Builder<E, K>) super.withElements(elements);
-		}
+        //-- Keys
 
+        /**
+         * Determines whether null elements are allowed or not.
+         * A null element will have a null key.
+         *
+         * @param nullable  true to allow null elements, false to disallow
+         * @return          this (for use in fluent interfaces)
+         */
+        public Builder<E,K> withKeyNull(boolean nullable) {
+            return withKeyNull(nullable ? NullMode.NORMAL : NullMode.NONE);
+        }
+
+        public Builder<E,K> withKeyNull(NullMode nullMode) {
+        	getKeyMapBuilder().nullMode = nullMode;
+        	allowNullElem = (nullMode == NullMode.NONE ? false : true);
+            return this;
+        }
+
+        /**
+         * Determines whether duplicates are allowed or not.
+         *
+         * @param duplicates    true to allow duplicates, false to disallow
+         * @return              this (for use in fluent interfaces)
+         */
+        public Builder<E,K> withKeyDuplicates(DuplicateMode duplicateMode) {
+        	getKeyMapBuilder().duplicateMode = duplicateMode;
+            return this;
+        }
+
+        /**
+         * Determines whether list should be sorted or not.
+         *
+         * @return              this (for use in fluent interfaces)
+         */
+        public Builder<E,K> withKeySort() {
+            return withKeySort(true);
+        }
+
+        /**
+         * Determines that list should be sorted.
+         *
+         * @param sort    true to sort list, otherwise false
+         * @return        this (for use in fluent interfaces)
+         */
+        public Builder<E,K> withKeySort(boolean sort) {
+        	getKeyMapBuilder().sort = sort;
+            return this;
+        }
+
+        /**
+         * Set comparator to use for sorting.
+         *
+         * @param comparator    comparator to use for sorting
+         * @return              this (for use in fluent interfaces)
+         */
+        public Builder<E,K> withKeyComparator(Comparator<? super K> comparator) {
+            return withKeyComparator(comparator, false);
+        }
+
+        /**
+         * Set comparator to use for sorting.
+         *
+         * @param comparator            comparator to use for sorting
+         * @param comparatorSortsNull   true if comparator sorts null, false if not
+         * @return                      this (for use in fluent interfaces)
+         */
+        public Builder<E,K> withKeyComparator(Comparator<? super K> comparator, boolean comparatorSortsNull) {
+        	KeyMapBuilder<E, Object> kmb = getKeyMapBuilder();
+        	kmb.comparator = comparator;
+            kmb.comparatorSortsNull = comparatorSortsNull;
+            return this;
+        }
+
+        /**
+         * Determines that nulls are sorted first.
+         *
+         * @return  this (for use in fluent interfaces)
+         */
+        public Builder<E,K> withKeyNullsFirst() {
+            return withKeyNullsFirst(true);
+        }
+
+        /**
+         * Determines whether nulls are sorted first or last.
+         *
+         * @param nullsFirst    true to sort nulls first, false to sort nulls last
+         * @return              this (for use in fluent interfaces)
+         */
+        public Builder<E,K> withKeyNullsFirst(boolean nullsFirst) {
+        	getKeyMapBuilder().sortNullsFirst = nullsFirst;
+            return this;
+        }
+
+        /**
+         * @return
+         */
+        public MapList<E,K> build() {
+        	if (keyList == null) {
+               	keyList = new MapList<E,K>(false);
+        	}
+        	build(keyList);
+        	return (MapList<E,K>) keyList;
+        }
     }
-
 
     /** UID for serialization. */
     private static final long serialVersionUID = -927503847084195522L;
@@ -150,6 +264,7 @@ public class MapList<E, K> extends KeyList<E> {
      * Internal use in builder and child classes only.
      */
     protected MapList(boolean ignore) {
+    	super(ignore);
     }
 
     /**
@@ -160,6 +275,10 @@ public class MapList<E, K> extends KeyList<E> {
      */
     MapList(MapList<E, K> that) {
         super(that);
+    }
+
+    public Mapper<E,K> getMapper() {
+    	return (Mapper<E, K>) keyMaps[0].mapper;
     }
 
     /**
@@ -176,6 +295,58 @@ public class MapList<E, K> extends KeyList<E> {
     public static <E, K> MapList.Builder<E, K> builder(Mapper<E, K> mapper) {
         return new MapList.Builder<E, K>(mapper);
     }
+
+    //-- Key methods
+
+    public boolean containsKey(K key) {
+    	return super.containsKey(0, key);
+    }
+
+    public int indexOfKey(K key) {
+    	return super.indexOfKey(0, key);
+    }
+
+	public int getCountDistinctKeys() {
+		return super.getCountDistinctKeys(0);
+	}
+
+	public GapList<Object> getDistinctKeys() {
+		return super.getDistinctKeys(0);
+	}
+
+	public E getByKey(K key) {
+		return super.getByKey(0, key);
+	}
+
+	public GapList<E> getAllByKey(K key) {
+		return super.getAllByKey(0, key);
+	}
+
+	public int getCountByKey(K key) {
+		return super.getCountByKey(0, key);
+	}
+
+	public E removeByKey(K key) {
+		return super.removeByKey(0, key);
+	}
+
+	public GapList<E> removeAllByKey(K key) {
+		return super.removeAllByKey(0, key);
+	}
+
+	public E put(E elem) {
+		K key = getMapper().getKey(elem);
+		int index = indexOfKey(0, key);
+		E oldElem;
+		if (index == -1) {
+			oldElem = null;
+			add(elem);
+		} else {
+			oldElem = get(index);
+			set(index, elem);
+		}
+		return oldElem;
+	}
 
     @Override
     public MapList<E, K> copy() {
@@ -194,10 +365,6 @@ public class MapList<E, K> extends KeyList<E> {
         MapList<E, K> copy = new MapList<E, K>(this);
         copy.initCrop(this);
         return copy;
-    }
-
-    public Mapper<E, K> getMapper() {
-        return (Mapper<E, K>) getKeyMap(0).mapper;
     }
 
     /**
