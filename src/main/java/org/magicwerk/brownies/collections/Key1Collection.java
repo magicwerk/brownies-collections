f08ec861e07a28542b9ffcfc817ca738d59227db
magicwerk <magicwerk@gmail.com>
2022-04-01 15:17:24 +0200

    FileTypes

--- src/main/java/org/magicwerk/brownies/collections/Key1Collection.java
+++ src/main/java/org/magicwerk/brownies/collections/Key1Collection.java
@@ -202,8 +202,7 @@ public class Key1Collection<E, K> extends KeyCollectionImpl<E> {
 
 		/**
 		 * Specify this key to be a primary key.
-		 * This is identical to calling
-		 * withKey1Map(mapper), withKey1Null(false), and withKey1Duplicates(false).
+		 * This is identical to calling withKey1Map(mapper), withKey1Null(false), and withKey1Duplicates(false).
 		 *
 		 * @param mapper	mapper to use
 		 * @return			this (fluent interface)
@@ -214,8 +213,7 @@ public class Key1Collection<E, K> extends KeyCollectionImpl<E> {
 
 		/**
 		 * Specify this key to be a unique key.
-		 * This is identical to calling
-		 * withKey1Map(mapper), withKey1Null(true), and withKey1Duplicates(false, true).
+		 * This is identical to calling withKey1Map(mapper), withKey1Null(true), and withKey1Duplicates(false, true).
 		 *
 		 * @param mapper	mapper to use
 		 * @return			this (fluent interface)
